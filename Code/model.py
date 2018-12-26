383c050701e4142a94b1cf4ec25263a76c31f410
lsa1997 <31985749+lsa1997@users.noreply.github.com>
2018-12-26 21:48:34 +0800

    update
    函数结构有所修改，将原Loss拆分为prediction和iteration

--- Code/model.py
+++ Code/model.py
@@ -1,16 +1,19 @@
 # coding = utf-8
-
 import numpy as np
 from layers import *
 
 class cnn_AlexNet(object):
-    def __init__(self, batch_size, input_dim=(3, 32, 32), num_filters=(32), filter_size=(7),
+    def __init__(self, batch_size, input_dim=(3, 64, 64), num_filters=(128), filter_size=(3),
                  hidden_dim=100, num_classes=20, weight_scale=1e-3, reg=0.0,
                  dtype=np.float32):
             
         self.batch_size = batch_size
         C, H, W = input_dim 
         F1, HH1, WW1 = num_filters[0], filter_size[0], filter_size[0]
+
+        self.fc_cache1 = ()
+        self.fc_cache2 = ()
+        self.pool_cache1 = ()
 
         self.params = {}
         self.params['w_conv1'] = weight_scale * np.random.randn(F1, C, HH1, WW1)
@@ -29,7 +32,7 @@ class cnn_AlexNet(object):
     def restore(self, dir):
         pass
 
-    def loss(self, x, y=None):
+    def forward(self, x):
         w_conv1, b_conv1 = self.params['w_conv1'], self.params['b_conv1']
         w_fc1, b_fc1 = self.params['w_fc1'], self.params['b_fc1']
         w_fc2, b_fc2 = self.params['w_fc2'], self.params['b_fc2']
@@ -39,20 +42,22 @@ class cnn_AlexNet(object):
         pool_param = {'height': 2, 'width': 2, 'stride': 2}
 
         #forward
-        pool_out1, pool_cache1 = conv_relu_pool_forward(x, w_conv1, b_conv1, conv_param, pool_param)
+        pool_out1, self.pool_cache1 = conv_relu_pool_forward(x, w_conv1, b_conv1, conv_param, pool_param)
 
-        fc_out1, fc_cache1 = fc_relu_forward(pool_out1, w_fc1, b_fc1)
-        fc_out2, fc_cache2 = fc_forward(fc_out1, w_fc2, b_fc2)
+        fc_out1, self.fc_cache1 = fc_relu_forward(pool_out1, w_fc1, b_fc1)
+        fc_out2, self.fc_cache2 = fc_forward(fc_out1, w_fc2, b_fc2)
 
-        if y is None:
-            return softmax_forward(fc_out2)
+        return fc_out2
         
+    def iteration(self, x, y):
+        #forward
+        fc_out2 = self.forward(x)
         #backward
         loss, grads = 0, {}
         loss, dout = softmax_loss(fc_out2, y)
-        dx_fc2, grads['w_fc2'], grads['b_fc2'] = fc_backward(dout, fc_cache2)
-        dx_fc1, grads['w_fc1'], grads['b_fc1'] = fc_relu_backward(dx_fc2, fc_cache1)
-        dx_conv1, grads['w_conv1'], grads['b_conv1'] = conv_relu_pool_backward(dx_fc1, pool_cache1)
+        dx_fc2, grads['w_fc2'], grads['b_fc2'] = fc_backward(dout, self.fc_cache2)
+        dx_fc1, grads['w_fc1'], grads['b_fc1'] = fc_relu_backward(dx_fc2, self.fc_cache1)
+        dx_conv1, grads['w_conv1'], grads['b_conv1'] = conv_relu_pool_backward(dx_fc1, self.pool_cache1)
 
         return loss, grads
 
