26cf9952de0d5cc82fb45753fe87cafa82332720
Weilun Wang <250501715@qq.com>
2018-12-28 16:35:51 +0800

    Update train.py

--- Code/train.py
+++ Code/train.py
@@ -1,12 +1,30 @@
-from .model import cnn_AlexNet
 from .data_load import *
+from .model import cnn_AlexNet
+from .update import adam
+
+
+def check_accuracy(model, x_batch, y_batch, batch_size=100):
+    accuary = 0
+    correct_num = 0
+    N = x_batch.shape[0] * batch_size
+    for i in range(N):
+        scores, _ = model.forward(x_batch[i])
+        top_three = np.argsort(scores)[:, -3:]
+        correct_num += np.sum((y_batch - 1)[i] == top_three)
+        
+    accuary = correct_num / N
+    return accuary
 
 # Define dir and hyperparam
 batch_size = 50
-epoches = 40
+epochs = 40
 steps = 200
 data_dir = '../dataset'
 saving_dir = '../model'
+adam_params = {}
+loss_history = []
+train_acc_history = []
+lr_decay = 0.95
 
 # Load and Process dataset
 print('load and process dataset...')
@@ -19,14 +37,30 @@ alexnet = cnn_AlexNet(batch_size)
 
 # Training
 print('start training...')
-for epoch in range(epoches):
-    print('epoch: %d' % epoch)
+for epoch in range(epochs):
     for step in range(steps):
-        loss = alexnet.iteration(images_batch[step], y_labels_batch[step])
+        loss, grads = alexnet.iteration(images_batch[step], y_labels_batch[step])
+        loss_history.append(loss)
         print('epoch: %d , ' % epoch + 'step: %d , ' % step + 'Loss: %f' % loss)
+        
+        #Update parameters
+        for p, w in alexnet.params.items():
+            dw = grads[p]
+            alexnet.params[p], adam_params[p] = adam(w,dw,adam_params[p])
+    
+    #decay the learning rate
+    for p in adam_params:
+        adam_params[p]['learning_rate'] *= lr_decay
+
+    #check accuary
+    train_acc = check_accuracy(alexnet, images_batch, y_labels_batch, batch_size=batch_size)
+    train_acc_history.append(train_acc)
+    print('Epoch %d/%d, train_acc: %f'%(
+        epoch + 1, epochs, train_acc))
+        
+    #save parameters
+    alexnet.saving(saving_dir)
+
 print('finish training.')
 alexnet.saving(saving_dir)
 print('model saved.')
-
-
-
